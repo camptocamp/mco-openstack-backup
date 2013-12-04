1121b9e51de81acb2f5a7085e99ad251f8ba39eb
Raphaël Pinson <raphael.pinson@camptocamp.com>
2013-12-04 17:38:16 +0100

    Avoid having two rpc requests at the same time

--- openstack_backup.rb
+++ openstack_backup.rb
@@ -3,25 +3,22 @@
 require 'mcollective'
 include MCollective::RPC
 
-options = rpcoptions do |parser, options|
-  parser.define_head "OpenStack Backup Client"
-  parser.banner = "Usage: openstack_backup [options]"
-  
-  parser.on('-u', '--unfreeze-after SECONDS', 'Unfreeze after n seconds') do |v|
-    options[:unfreeze_after] = v.to_i
+mc = rpcclient('fsfreeze')
+mc.discovery_method = 'mc'
+
+all_nodes = mc.discover.clone
+
+all_nodes.in_groups_of(5) do |nodes|
+  mc.reset
+  mc.discover :nodes => nodes.compact
+
+  mc.fsfreeze(:filesystem => 'ALL') do |resp|
+    puts "Frozen: #{resp[:senderid]}"
+    puts "Launching backup for #{resp[:senderid]}"
+    puts "Unfreezing #{resp[:senderid]}"
+    mc.reset
+    mc.identity_filter resp[:senderid]
+    mc.fsunfreeze(:filesystem => 'ALL')
   end
-end
-
-options[:unfreeze_after] ||= 30
-
-fsfreeze = rpcclient('fsfreeze')
-
-fsfreeze.fsfreeze(:filesystem => 'ALL', :unfreeze_after => options[:unfreeze_after]) do |resp|
-  puts "Frozen: #{resp[:senderid]}"
-  puts "Launching backup for #{resp[:senderid]}"
-  puts "Unfreezing #{resp[:senderid]}"
-  fsfreeze.reset
-  fsfreeze.identity_filter resp[:senderid]
-  fsfreeze.fsunfreeze(:filesystem => 'ALL')
 end
 
